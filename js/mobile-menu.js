2acf4a181bdefb7750fc98dd9c632e816c82a682
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2023-04-12 18:46:13 +0300

    fox2.1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,4 @@
-(() => {
+а(() => {
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
